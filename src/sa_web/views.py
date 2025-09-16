7d4b89221d4e4d04289e62f41c8676cd32b3612d
Mjumbe Poe <mjumbewu@gmail.com>
2025-09-15 22:31:29 -0400

    Do not forward API server csrf tokens to client

--- src/sa_web/views.py
+++ src/sa_web/views.py
@@ -279,9 +279,16 @@ def proxy_view(request, url, requests_args={}):
     # For full URLs, use a real proxy.
     if url.startswith('http:') or url.startswith('https:'):
         response = remote_proxy_view(request, url, requests_args=requests_args)
+
+        # Do not pass on csrf cookies from proxied requests.
+        if 'csrftoken' in response.cookies:
+            del response.cookies['csrftoken']
+
+        # Ensure that cookies set from proxied requests are samesite=None
         for cookie in response.cookies.values():
             cookie['samesite'] = 'None'
             cookie['secure'] = True
+
         return response
 
     # For local paths, use a simpler proxy. If there are headers specified
